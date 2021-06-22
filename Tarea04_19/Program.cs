4aa531a9464301d27de471b38849faaae133b016
Carlos <Carlos@xtrene.com>
2021-06-22 13:22:38 +0200

    Agregar archivos de proyecto.

---
+++ Tarea04_19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tarea04_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enunciado: Muestra los números del 1 al 100 (ambos incluidos) divisibles entre 2 y 3. Utiliza el bucle que desees.
+
+            // Asumiendo que el número ha de cumplir ambas condiciones (entre 2 y entre 3) 
+            Console.WriteLine("Los números del 1 al 100 divisibles entre 2 Y 3: ");
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 == 0 && i % 3 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            // Asumiendo que el número ha de cumplir alguna de las condiciones (entre 2 o entre 3) 
+            Console.WriteLine("Los números del 1 al 100 divisibles entre 2 o entre 3: ");
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 2 == 0 || i % 3 == 0)
+                {
+                    Console.Write(" " + i);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
